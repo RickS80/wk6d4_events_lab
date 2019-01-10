453a3e1c0e14ea8cb3fbfdd94cb1db6e76f623cf
Rick Symington <rick.symington@gmail.com>
2019-01-10 15:55:08 +0000

    Add title to reading list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+  // console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form')
+  form.addEventListener('submit', handleFormSubmit)
+});
+
+const handleFormSubmit = function (event){
+  event.preventDefault();
+  console.log(event.target.title.value);
+  console.log(event.target.author.value);
+  console.log(event.target.category.value);
+
+const createListTitle = document.createElement('p')
+createListTitle.textContent = `Book Title: ${event.target.title.value}`
+
+const list = document.querySelector('#reading-list')
+list.appendChild(createListTitle)
+
+}
+//   resultForm = document.querySelector('save')
+//   resultForm.textContent = 'testing'
+// }
+//
+// const createListItem = function(event) {
+//   const newReadingListTitle = document.createElement('li')
+//   newReadingListTitle.textContent = `title`
+//   const newReadingAuthor = document.createElement('li')
+//   newReadingListTitle.textContent = `author`
+// }
+
+
+// const list = document.querySelector('#reading-list')
+// list.appendChild(createListItem)
