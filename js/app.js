a9d9ead8f6c606788f3c69489f596294d76dc4c1
Rick Symington <rick.symington@gmail.com>
2019-01-10 16:14:57 +0000

    Reading list submission works

--- js/app.js
+++ js/app.js
@@ -11,11 +11,26 @@ const handleFormSubmit = function (event){
   console.log(event.target.author.value);
   console.log(event.target.category.value);
 
-const createListTitle = document.createElement('p')
-createListTitle.textContent = `Book Title: ${event.target.title.value}`
+
+const listItem = document.createElement('div')
+
+
+  const createListTitle = document.createElement('p')
+  createListTitle.textContent = `Title: ${event.target.title.value}`
+  listItem.appendChild(createListTitle)
+
+  const createListAuthor = document.createElement('p')
+  createListAuthor.textContent = `Author: ${event.target.author.value}`
+  listItem.appendChild(createListAuthor)
+
+  const createListCategory = document.createElement('p')
+  createListCategory.textContent = `Category: ${event.target.category.value}`
+  listItem.appendChild(createListCategory)
+
 
 const list = document.querySelector('#reading-list')
-list.appendChild(createListTitle)
+list.appendChild(listItem)
+
 
 }
 //   resultForm = document.querySelector('save')
